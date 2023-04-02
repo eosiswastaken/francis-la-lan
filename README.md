83e22e2bd6afa23bb1f73e0af72b8472bc83ab82
Antoine Cerdan <antoinecerdan31@gmail.com>
2023-04-02 08:14:35 +0200

    db, create and join lans

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Discord bot for my LAN parties (self-hostable, I guess)
 Run it yourself :
 
 Open a console and enter into "env" venv
+env\Scripts\activate.bat
 
 Run main.py in the venv
 
--- bot/main.py
+++ bot/main.py
@@ -1,8 +1,21 @@
 ## IMPORTS
 
 import discord
-import os 
+import os
+import sqlite3
 from dotenv import load_dotenv
+import uuid
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -11,6 +24,41 @@ from dotenv import load_dotenv
 load_dotenv() # load all the variables from the env file
 bot = discord.Bot()
 
+conn = sqlite3.connect("francis.db")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## CLASSES AND HELPERS
+
+
+
+def get_lans(ctx):
+    sql = "SELECT name FROM lan"
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    print(data[0])
+    print(data[0][0])
+    return data[0]
+
+
+
+
+
+
 
 
 ## BOT CODE
@@ -18,10 +66,57 @@ bot = discord.Bot()
 @bot.event
 async def on_ready():
     print(f"{bot.user} is ready and online!")
+    cursor = conn.cursor()
+    cursor.execute("""
+            CREATE TABLE IF NOT EXISTS lan(
+                id INTEGER,
+                name TEXT,
+                description TEXT,
+                max_seats INTEGER)
+        """)
+    
+    cursor.execute("""
+            CREATE TABLE IF NOT EXISTS player(
+                id INTEGER,
+                lan_id INTEGER,
+                seat INTEGER,
+                games TEXT,
+                handles TEXT,
+                team TEXT)
+        """)
+
 
 @bot.slash_command(name = "hello", description = "Say hello to the bot")
 async def hello(ctx):
     await ctx.respond("Hey!")
+
+
+
+@bot.slash_command(name = "inscription", description = "Inscirs toi a une LAN")
+async def inscription(ctx,lan: discord.Option(str, autocomplete=discord.utils.basic_autocomplete(get_lans))):
+        await ctx.respond(f'Tu fais partie de la lan {lan} !')
+
+
+
+@bot.slash_command(name = "creer_lan", description = "Crée une nouvelle LAN")
+async def creer_lan(ctx,name:str,description:str,max_seats:int):
+        sql = "INSERT INTO lan VALUES (?, ?, ?, ?)"
+        cursor = conn.cursor()
+        cursor.execute(sql, [(str(uuid.uuid4())), (name), (description), (max_seats)])
+        conn.commit()
+
+        await ctx.respond(f'Nouvelle lan crée ! Nom : {name}, Description : {description}, Setups : {max_seats}')
+
+
+
+
+
+
+
+
+
+
+
 
 
 
