55aa5f3c0c1d268b51dfa8a293b2a5e66c596274
Antoine Cerdan <antoinecerdan31@gmail.com>
2023-04-02 11:31:45 +0200

    join, select seat and profile completed

--- bot/main.py
+++ bot/main.py
@@ -56,14 +56,65 @@ def get_lans(ctx):
 
 
 def get_lan_id(lan_name):
-    sql = "SELECT id FROM lan where name = ?"
+    sql = "SELECT id FROM lan WHERE name = ?"
     cursor = conn.cursor()
     cursor.execute(sql, [(lan_name)])
     data = cursor.fetchall()
     return data[0][0]
+
+
+def get_lan_name(lan_id):
+    sql = "SELECT name FROM lan WHERE id = ?"
+    cursor = conn.cursor()
+    cursor.execute(sql, [(lan_id)])
+    data = cursor.fetchall()
+    return data[0][0]
       
 
+def get_seats(ctx: discord.AutocompleteContext):
 
+    lan_name = ctx.options['lan_name']
+    lan_id = get_lan_id(lan_name)
+    print(f"fgreegergregergegege {lan_name}")
+
+    sql = "SELECT max_seats FROM lan WHERE id = ?"
+    cursor = conn.cursor()
+    cursor.execute(sql, [(lan_id)])
+    data = cursor.fetchall()
+    print(f"fewhufewifhuw{data}")
+    max_seats = data[0][0]
+    x = []
+    for i in range(1,max_seats+1):
+          x.append(str(i))
+
+    sql = "SELECT seat FROM player WHERE lan_id = ?"
+    print(lan_id)
+    cursor = conn.cursor()
+    cursor.execute(sql, [(lan_id)])
+    data = cursor.fetchall()
+    booked_seats = data[0]
+    print(f"booked : {booked_seats}")
+    for booked_seat in booked_seats:
+          if booked_seat != 0:
+            x.pop(x.index(str(booked_seat)))
+    return x
+
+
+def get_player_lan(id):
+    
+    sql = "SELECT lan_id FROM player WHERE id = ?"
+    cursor = conn.cursor()
+    cursor.execute(sql, [(id)])
+    data = cursor.fetchall()
+    return get_lan_name(data[0][0])
+
+def get_player_seat(id):
+    sql = "SELECT seat FROM player WHERE id = ?"
+    cursor = conn.cursor()
+    cursor.execute(sql, [(id)])
+    data = cursor.fetchall()
+    return data[0][0]
+    
 
 
 
@@ -119,21 +170,28 @@ async def creer_lan(ctx,name:str,description:str,max_seats:int):
         cursor.execute(sql, [(str(uuid.uuid4())), (name), (description), (max_seats)])
         conn.commit()
 
-        await ctx.respond(f'New LAN created ! Name : {name}, Description : {description}, Setups : {max_seats}')
+        await ctx.respond(f'New LAN created ! Name : {name}, Description : {description}, Seats : {max_seats}')
 
 
 
-@bot.slash_command(name = "select_setup", description = "Choose your setup")
-async def creer_lan(ctx,user:discord.Member):
-
-        await ctx.respond(f"{user.name}'s profile : \n ** -> ** LAN : LMAO NO \n **->** Setup : LMAO NO ")
+@bot.slash_command(name = "select_seat", description = "Choose your seat")
+async def creer_lan(ctx,lan_name: discord.Option(str, autocomplete=discord.utils.basic_autocomplete(get_lans)),seat: discord.Option(str, autocomplete=discord.utils.basic_autocomplete(get_seats))):
+        sql = "UPDATE player SET seat = ? WHERE id = ?"
+        print("ici")
+        cursor = conn.cursor()
+        print("ici2")
+        cursor.execute(sql, [(seat), (ctx.author.id)])
+        print("ici3")
+        conn.commit()
+        print("ici4")
+        await ctx.respond(f"ok2")
 
 
 
 @bot.slash_command(name = "profile", description = "Check anyone's profile")
 async def creer_lan(ctx,user:discord.Member):
 
-        await ctx.respond(f"{user.name}'s profile : \n ** -> ** LAN : LMAO NO \n **->** Setup : LMAO NO ")
+        await ctx.respond(f"{user.name}'s profile : \n ** -> ** LAN : {get_player_lan(ctx.author.id)} \n **->** Seat : {get_player_seat(ctx.author.id)} ")
 
 
 
